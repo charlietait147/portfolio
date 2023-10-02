3cbf9badc4ed5e656c6422382624ab846a44ec3c
Charlie Tait <charlietait147@gmail.com>
2023-10-02 17:52:49 +0100

    clean up styling

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -4,8 +4,8 @@ function Hero() {
         <section className = "hero">
             <div className="hero__wrapper">
                     <p className="hero__welcome">Hi Everyone!</p>
-                    <h1 className="hero__tagline">I'm Charlie <span className = "hero__tagline hero__tagline--bold">Tait</span></h1>
-                    <p className="hero__description">I'm a <span className = "hero__description hero__description--bold">full stack developer</span>, undergraduate of criminology and master's graduate of cybersecurity, excited to write excellent quality code to increase performance and productivity. </p>
+                    <h1 className="hero__tagline">I'm Charlie <strong className = "hero__tagline hero__tagline--bold">Tait</strong></h1>
+                    <p className="hero__description">I'm a <strong className = "hero__description hero__description--bold">full stack developer</strong>, undergraduate of criminology and master's graduate of cybersecurity, excited to write excellent quality code to increase performance and productivity. </p>
             </div>
         </section>
      );
