3a3e17bc1596e7bf40831561a22b5fcde1de149c
Charlie Tait <charlietait147@gmail.com>
2023-10-02 10:31:04 +0100

    add content and styles on header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,22 @@
 import Hero from "../Hero/Hero";
 import NavBar from "../NavBar/NavBar";
 import "./Header.scss";
+import portfolioImage from "../../public/assets/images/portfolio-edited-image.png";
+import Image from "next/image"
+
 function Header() {
     return ( 
-        <div>
-        <NavBar />
-        <Hero />
-        </div>
+        <header className="header">
+            <NavBar />
+            <div className="header__container">
+                <div className="header__text-container">
+                    <Hero />
+                </div>
+                <div className="header__image-container">
+                    <Image src =  {portfolioImage} className = "header__image" alt = "portfolio image" />
+                </div>
+            </div>
+        </header>
      );
 }
 
