dd4564d77c69d4e8f55c35e629f36728dfaa883e
Charlie Tait <charlietait147@gmail.com>
2023-10-02 17:27:58 +0100

    add hero content and styling

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,7 +1,13 @@
 import "./Hero.scss";
 function Hero() {
     return ( 
-        <div>Hero section</div>
+        <section className = "hero">
+            <div className="hero__wrapper">
+                    <p className="hero__welcome">Hi Everyone!</p>
+                    <h1 className="hero__tagline">I'm Charlie <span className = "hero__tagline hero__tagline--bold">Tait</span></h1>
+                    <p className="hero__description">I'm a <span className = "hero__description hero__description--bold">full stack developer</span>, undergraduate of criminology and master's graduate of cybersecurity, excited to write excellent quality code to increase performance and productivity. </p>
+            </div>
+        </section>
      );
 }
 
