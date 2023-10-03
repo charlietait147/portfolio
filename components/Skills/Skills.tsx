4c7c4b6ef1193b05be80823c905a60939e66c094
Charlie Tait <charlietait147@gmail.com>
2023-10-03 12:05:13 +0100

    install coreui icons, import icons and add skills content and styles

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -1,6 +1,27 @@
+"use client"
+
+import "./Skills.scss";
+import  CIcon from "@coreui/icons-react";
+import {cibNextJs, cibSassAlt, cibReact, cibPostman, cibMysql, cibGit, cibHtml5, cibTypescript} from "@coreui/icons";
+
 function Skills() {
     return ( 
-        <div>Skills section</div>
+        <section className="skills">
+            <div className="skills__title-container">
+                <h3 className="skills__title">Skills</h3>
+            </div>
+            <div className="skills__icons-container">
+                <CIcon icon={cibHtml5} className="skills__icon" />
+                <CIcon icon={cibSassAlt} className="skills__icon" />
+                <CIcon icon={cibReact} className="skills__icon" />
+                <CIcon icon={cibGit} className="skills__icon" />
+                <CIcon icon={cibPostman} className="skills__icon" />
+                <CIcon icon={cibMysql} className="skills__icon" />
+                <CIcon icon={cibNextJs} className="skills__icon" />
+                <CIcon icon={cibTypescript} className="skills__icon" />
+            </div>
+
+        </section>
      );
 }
 
