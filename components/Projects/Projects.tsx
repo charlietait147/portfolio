814f5342f7adc3d1b05b5f93f2a2666b9bb14f27
Charlie Tait <charlietait147@gmail.com>
2023-10-03 16:14:37 +0100

    import home ecoearner image, add project content and styles

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,8 +1,34 @@
 import "./Projects.scss";
+import Image from "next/image";
+import homeEcoEarnerImg from "../../public/assets/images/HomeEcoEarner.png";
+
 function Projects() {
-    return (
-        <div>Project section</div>
-     );
+  return (
+    <section className="projects">
+      <div className="projects__title-container">
+        <h3 className="projects__title">Projects</h3>
+      </div>
+      <div className="projects__projects-container">
+        <Image
+          className="projects__home-eco"
+          src={homeEcoEarnerImg}
+          alt="HomeEcoEarner project snippet"
+        />
+        <div className="projects__text-container">
+          <h4 className="projects__text-title">
+            Home{" "}
+            <span className="projects__text-title projects__text-title--green">
+              EcoEarner
+            </span>
+          </h4>
+          <p className="projects__text-description">
+            A development landing page and website of a start-up renewable
+            energy company.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
