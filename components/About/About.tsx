b3a9dfb0037589f64d1b09536e2bca1f82026a11
Charlie Tait <charlietait147@gmail.com>
2023-10-03 10:53:50 +0100

    add about section content and styles

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,8 +1,19 @@
-import "./About.scss"
+import "./About.scss";
 function About() {
-    return (
-        <div>About section</div>
-      );
+  return (
+    <section className="about">
+      <div className="about__title-container">
+        <h3 className="about__title">About Me</h3>
+      </div>
+      <p className="about__description">
+        Coming from an educational background in criminology and cyber-security,
+        I have acquired a strong knowledge base in computing and a talent for
+        problem solving as well as a passion for technology as a whole, with a
+        particular interest in security technology and cyber-crime. I am excited
+        to take my skills into software engineering.
+      </p>
+    </section>
+  );
 }
 
-export default About
+export default About;
