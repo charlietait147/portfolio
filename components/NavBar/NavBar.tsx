6bb95614a1cdaa10110acf5c6b8692e35fbc1d1a
Charlie Tait <charlietait147@gmail.com>
2023-10-15 16:11:06 +0100

    add logo

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,9 +1,12 @@
 import "./NavBar.scss";
+import Logo from "../../public/assets/logos/Charlie_Tait.png";
+import Image from "next/image";
+import Link from "next/link";
 function NavBar() {
     return (
         <nav className = "nav">
             <div className="nav__wrapper">
-                <a href="/" className="nav__logo">Charlie.</a>
+                <Link href = "/"><Image src = {Logo} alt = "logo" className="nav__logo" /></Link>
             </div>
         </nav>
       );
