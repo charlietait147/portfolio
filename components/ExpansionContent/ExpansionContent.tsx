0b49ba7d8ef5eeae82fd12c8cd1bac1fd9554b36
Charlie Tait <charlietait147@gmail.com>
2023-12-08 14:27:56 +0000

    add challenges section and styling

--- components/ExpansionContent/ExpansionContent.tsx
+++ components/ExpansionContent/ExpansionContent.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import expansionImg from "../../public/assets/images/expansion-edited.png";
 import CIcon from "@coreui/icons-react";
@@ -11,11 +11,12 @@ function ExpansionContent() {
       <header className="expansion-content__header">
         <div className="expansion-content__header-wrapper">
           <div className="expansion-content__title-container">
-          <h1 className="expansion-content__title">expansion</h1>
+            <h1 className="expansion-content__title">expansion</h1>
           </div>
           <p className="expansion-content__tagline">
             {" "}
-            A thesaurus app powered by Thesaurus API. It fetches a list of 12 synonyms for words with colour co-ordinations.
+            A thesaurus app powered by Thesaurus API. It fetches a list of 12
+            synonyms for words with colour co-ordinations.
           </p>
           <div className="expansion-content__main-image-container">
             <Image
@@ -68,18 +69,50 @@ function ExpansionContent() {
         </ul>
       </section>
       <section id="background" className="expansion-content__section">
-      <h2 className="expansion-content__section-title">Background</h2>
-      <p className="expansion-content__text">
-         I wanted to challenge myself by using both Typescript to fetch data from an API and Tailwind CSS to design a nice user interface. I decided to create a new Thesaurus app, using the Thesaurus API from API Ninjas. 
+        <h2 className="expansion-content__section-title">Background</h2>
+        <p className="expansion-content__text">
+          I wanted to challenge myself by using both Typescript to fetch data
+          from an API and Tailwind CSS to design a nice user interface. I
+          decided to create a new Thesaurus app, using the Thesaurus API from
+          API Ninjas.
         </p>
       </section>
       <section id="challenges" className="expansion-content__section">
-      <h2 className="expansion-content__section-title">Challenges</h2>
-      <h4 className="expansion-content__subtitle">Using TypeScript with Axios</h4>
-      <li className="project-content__text">
-        1
-      </li>
-
+        <h2 className="expansion-content__section-title">Challenges</h2>
+        <h4 className="expansion-content__subtitle">
+          Using TypeScript with Axios and event handlers
+        </h4>
+        <ul className="expansion-content__section-list">
+          <li className="expansion-content__text">
+            Being used to working with plain JavaScript, I had to research the
+            structure of API requests with Typescript, after receiving
+            declaration errors. After research, I declared an interface with two
+            of the values I wanted to fetch, the word and a synonyms array. I
+            defined their type in the interface before putting the interface
+            into state, so I could access it to display content on the page. I
+            ensured that this could also be null, given that there would be no
+            data before the API request had been made. I further explicity
+            attached the interface to the get request.
+          </li>
+          <br />
+          <li className="expansion-content__text">
+            I also ensured that I specifically defined the search bar submit
+            event, by importing React's form event. To handle the onChange
+            event, I imported React's Change Event.
+          </li>
+        </ul>
+        <h4 className="expansion-content__subtitle">
+          Clearing the search input
+        </h4>
+        <ul className="expansion-content__section-list">
+          <li className="expansion-content__text">
+            I had struggled with finding the right function, in my believing
+            that the trim function removed all characters and whitespace, to
+            finding that it only removed whitespace. After some research, I
+            found the replace function was suitable, in replacing lowercase and
+            uppercase characters and whitespace with an empty string.
+          </li>
+        </ul>
       </section>
     </main>
   );
