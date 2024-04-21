5df4ac607e2f4d1f41f82c8908922163e8614a1e
Charlie Tait <charlietait147@gmail.com>
2024-04-21 15:51:33 +0100

    add github, java, mongod and wordpress icons

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -2,7 +2,7 @@
 
 import "./Skills.scss";
 import  CIcon from "@coreui/icons-react";
-import {cibNextJs, cibSassAlt, cibReact, cibPostman, cibMysql, cibGit, cibHtml5, cibTypescript} from "@coreui/icons";
+import {cibNextJs, cibSassAlt, cibReact, cibPostman, cibMysql, cibGit, cibHtml5, cibTypescript, cibJava, cibMongodb, cibTrello, cibGithub, cibWordpress, cibJavascript} from "@coreui/icons";
 
 function Skills() {
     return ( 
@@ -13,12 +13,17 @@ function Skills() {
             <div className="skills__icons-container">
                 <CIcon icon={cibHtml5} className="skills__icon" />
                 <CIcon icon={cibSassAlt} className="skills__icon" />
-                <CIcon icon={cibReact} className="skills__icon" />
-                <CIcon icon={cibGit} className="skills__icon" />
-                <CIcon icon={cibPostman} className="skills__icon" />
-                <CIcon icon={cibMysql} className="skills__icon" />
-                <CIcon icon={cibNextJs} className="skills__icon" />
                 <CIcon icon={cibTypescript} className="skills__icon" />
+                <CIcon icon={cibReact} className="skills__icon" />
+                <CIcon icon={cibNextJs} className="skills__icon" />
+                <CIcon icon={cibMysql} className="skills__icon" />
+                <CIcon icon={cibGit} className="skills__icon" />
+                <CIcon icon={cibGithub} className="skills__icon" />
+                <CIcon icon={cibPostman} className="skills__icon" />
+                <CIcon icon={cibJava} className="skills__icon" />
+                <CIcon icon={cibMongodb} className="skills__icon" />
+                <CIcon icon={cibWordpress} className="skills__icon" />
+               
             </div>
 
         </section>
