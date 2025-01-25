017cf5798049f9a3ddd68296d59e4f0251bd083e
Charlie Tait <charlietait147@gmail.com>
2025-01-25 12:51:37 +0000

    add swish tech container with styling

--- components/SwishContent/SwishContent.jsx
+++ components/SwishContent/SwishContent.jsx
@@ -27,6 +27,39 @@ function SwishContent() {
           </div>
         </div>
       </header>
+      <section id="tech-stack" className="swish-content__section">
+        <h2 className="swish-content__section-title">Tech Stack</h2>
+        <div className="swish-content__tech-container">
+          <ul className="swish-content__tech-list">
+            <li className="swish-content__tech-item">Next.js</li>
+            <li className="swish-content__tech-item">Tailwind CSS</li>
+            <li className="swish-content__tech-item">Node JS</li>
+            <li className="swish-content__tech-item">Mongoose</li>
+            <li className="swish-content__tech-item">MongoDB</li>
+            <li className="swish-content__tech-item">Jest</li>
+            <li className="swish-content__tech-item">
+              React Testing Library
+            </li>
+            <li className="swish-content__tech-item">Mocha</li>
+            <li className="swish-content__tech-item">Chai</li>
+          </ul>
+        </div>
+        <ul className="swish-content__links-list">
+          <div className="swish-content__link-container">
+            <li className="swish-content__item">
+              <Link
+                className="swish-content__link"
+                href="https://github.com/charlietait147/swish"
+              >
+                GitHub
+              </Link>
+            </li>
+            <Link href="https://github.com/charlietait147/swish">
+              <CIcon icon={cibGithub} className="swish-content__icon" />
+            </Link>
+          </div>
+        </ul>
+      </section>
     </main>
   );
 }
