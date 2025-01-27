f398246338eeee4b2c24ce76662f9bcbf88e6b43
Charlie Tait <charlietait147@gmail.com>
2025-01-27 16:10:34 +0000

    add image of save modal to screenshots, add challenges section to swish project page

--- components/SwishContent/SwishContent.jsx
+++ components/SwishContent/SwishContent.jsx
@@ -6,8 +6,9 @@ import CIcon from "@coreui/icons-react";
 import { cibGithub } from "@coreui/icons";
 import swishImg from "../../public/assets/images/swish-app-screenshot.png";
 import signInModal from "../../public/assets/images/swish-sign-in-modal.png";
-import signUpForm from "../../public/assets/images/swish-sign-in-form.png"
+import signUpForm from "../../public/assets/images/swish-sign-in-form.png";
 import cafePage from "../../public/assets/images/swish-cafe-page.png";
+import saveModal from "../../public/assets/images/swish-saved-cafe-modal.png";
 import cafePageReview from "../../public/assets/images/swish-posting-review.png";
 import desktopDiscover from "../../public/assets/images/swish-desktop-discover.png";
 import mobileDiscover from "../../public/assets/images/swish-mobile-discover.png";
@@ -24,7 +25,7 @@ function SwishContent() {
           </div>
           <p className="swish-content__tagline">
             A database-driven café recommendation engine featuring user
-            registration, reviews and favourite cafes
+            registration, reviews and favourite cafés
           </p>
           <div className="swish-content__main-image-container">
             <Image
@@ -72,28 +73,119 @@ function SwishContent() {
           I wanted to leverage my skills in Next.js and Tailwind CSS to design
           an attractive, user-friendly café recommendation and review platform.
           I drew on my previous experience using MongoDB to build a database to
-          manage users, cafés and reviews. To refine my testing abilities, I
-          adopted a test-driven approach, in constructing tests for front-end
-          components, services, back-end unit tests for MongoDB collections and
-          integration tests to ensure seamless communication between the
-          front-end client and back-end server. In the future, I am excited to
+          manage users, cafés and reviews. In the future, I am excited to
           develop a mobile application that incorporates the design elements of
           the web platform and features discounts for customers to use,
           enhancing user engagement.
+          {/* To refine my testing abilities, I
+          adopted a test-driven approach, in constructing tests for front-end
+          components, services, back-end unit tests for MongoDB collections and
+          integration tests to ensure seamless communication between the
+          front-end client and back-end server. */}
         </p>
       </section>
       <section id="screenshots" className="swish-content__section">
         <h2 className="swish-content__section-title">Screenshots</h2>
         <div className="swish-content__screenshot-container">
-            <Image src={signInModal} alt="screenshot of login modal" className="swish-content__screenshot" />
-            <Image src={signUpForm} alt="screenshot of login form" className="swish-content__screenshot" />
-            <Image src={desktopDiscover} alt="screenshot of discovery page on desktop" className="swish-content__screenshot" />
-            <Image src={cafePage} alt="screenshot of cafe page on desktoop" className="swish-content__screenshot" />
-            <Image src={cafePageReview} alt="screenshot of review post" className="swish-content__screenshot" />
-            <Image src={mobileDiscover} alt="screenshot of discover page on mobile" className="swish-content__screenshot" />
-            <Image src={mobileFilters} alt="screenshot of filters on mobile" className="swish-content__screenshot" />
-            <Image src={accountInfo} alt="screenshot of account page" className="swish-content__screenshot" />
+          <Image
+            src={signInModal}
+            alt="screenshot of login modal"
+            className="swish-content__screenshot"
+          />
+          <Image
+            src={signUpForm}
+            alt="screenshot of login form"
+            className="swish-content__screenshot"
+          />
+          <Image
+            src={desktopDiscover}
+            alt="screenshot of discovery page on desktop"
+            className="swish-content__screenshot"
+          />
+          <Image
+            src={cafePage}
+            alt="screenshot of cafe page on desktop"
+            className="swish-content__screenshot"
+          />
+          <Image
+            src={cafePageReview}
+            alt="screenshot of review post"
+            className="swish-content__screenshot"
+          />
+          <Image
+            src={mobileDiscover}
+            alt="screenshot of discover page on mobile"
+            className="swish-content__screenshot"
+          />
+          <Image
+            src={mobileFilters}
+            alt="screenshot of filters on mobile"
+            className="swish-content__screenshot"
+          />
+          <Image
+            src={saveModal}
+            alt="screenshot of save modal"
+            className="swish-content__screenshot"
+          />
+          <Image
+            src={accountInfo}
+            alt="screenshot of account page"
+            className="swish-content__screenshot"
+          />
         </div>
+      </section>
+      <section id="challenges" className="swish-content__section">
+        <h2 className="swish-content__section-title">Challenges</h2>
+        <h4 className="swish-content__subtitle">Updating cafe data</h4>
+        <ul className="swish-content__section-list">
+          <li className="swish-content__text">
+            I faced a challenge in updating cafe data, for example, when adding
+            new properties such as a location and map to the JSON file of cafe
+            data. After saving these changes and running the command to seed the updated data, I discovered that all the
+            reviews for each cafe had been deleted.{" "}
+          </li>
+          <br />
+          <li className="swish-content__text">
+            To address this, I created an upsert script that destructured the
+            cafe data to exclude the reviews. This allowed me to update the cafe
+            data without affecting the reviews. I then ran the upsert script to
+            update the cafe data and the reviews remained intact.
+          </li>
+        </ul>
+        <h4 className="swish-content__subtitle">Filtering cafe amenities</h4>
+        <ul className="swish-content__section-list">
+          <li className="swish-content__text">
+            I encountered difficulty in implementing a range of amenity buttons
+            within a form. My goal was to allow users to select as many amenity
+            buttons as they wanted, with each selected button serving as a
+            filter to display cafes that offered the corresponding amenities.{" "}
+          </li>
+          <br />
+          <li className="swish-content__text">
+            I worked around this by creating a function that manages the state
+            of a list of selected amenities. The function checks if the clicked
+            amenity is already in the list and removes it it is and adds the
+            amenity to the list if it is not. I then passed this function to
+            each amenity button to manage the state of the selected amenities.
+          </li>
+        </ul>
+        <h4 className="swish-content__subtitle">Keeping a cafe saved</h4>
+        <ul className="swish-content__section-list">
+          <li className="swish-content__text">
+            I encountered a challenge in keeping a cafe saved when a user
+            navigated away from the cafe page. I wanted to ensure that when a
+            user saved a cafe, it would remain saved when they returned to the
+            page.{" "}
+          </li>
+          <br />
+          <li className="swish-content__text">
+            To successfully solve this, I created a service on the backend that
+            keeps track of which user is logged in and which cafe they want to
+            save. I then checked if the café belongs to that user and if it
+            does, I saved it to their list of favourite cafés. This ensured that
+            the cafe remained saved when the user navigated away from the page.
+          </li>
+        </ul>
       </section>
     </main>
   );
